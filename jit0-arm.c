8f039c33a61e1517369d2d1b274a863f509dbdf2
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-22 10:15:58 +0800

    jit0-arm: fix usage info
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- jit0-arm.c
+++ jit0-arm.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
   };
 
   if (argc < 2) {
-    fprintf(stderr, "Usage: jit1 <integer>\n");
+    fprintf(stderr, "Usage: jit0-arm <integer>\n");
     return 1;
   }
 
